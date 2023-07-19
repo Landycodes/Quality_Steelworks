79171e14f9b3e11290efcbfadbaf81f3e4663e5f
landycodes <landryandrewsk8@gmail.com>
2023-07-19 16:27:45 -0700

    changes to contact us form

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ Mobile menu functionality
 =======================================
 */
 
-$(".open-burger").on("click", function() {
-	$("#myNav").css("width", "100%");
+$(".open-burger").on("click", function () {
+  $("#myNav").css("width", "100%");
 });
 
-$(".close-burger").on("click", function() {
-	$("#myNav").css("width", "0%");
+$(".close-burger").on("click", function () {
+  $("#myNav").css("width", "0%");
 });
 
 /* 
@@ -22,31 +22,31 @@ var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-	showSlides((slideIndex += n));
+  showSlides((slideIndex += n));
 }
 
 function currentSlide(n) {
-	showSlides((slideIndex = n));
+  showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
-	var i;
-	var slides = document.getElementsByClassName("mySlides");
-	var dots = document.getElementsByClassName("dot");
-	if (n > slides.length) {
-		slideIndex = 1;
-	}
-	if (n < 1) {
-		slideIndex = slides.length;
-	}
-	for (i = 0; i < slides.length; i++) {
-		slides[i].style.display = "none";
-	}
-	for (i = 0; i < dots.length; i++) {
-		dots[i].className = dots[i].className.replace(" active", "");
-	}
-	slides[slideIndex - 1].style.display = "block";
-	dots[slideIndex - 1].className += " active";
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
 
 /* 
@@ -58,41 +58,70 @@ Scroll to anchor id's
 // Select all links with hashes
 
 $('a[href*="#"]')
-	// Remove links that don't actually link to anything
-	.not('[href="#"]')
-	.not('[href="#0"]')
-	.click(function(event) {
-		// On-page links
-		if (
-			location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") &&
-			location.hostname == this.hostname
-		) {
-			// Figure out element to scroll to
-			var target = $(this.hash);
-			target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
-			// Does a scroll target exist?
-			if (target.length) {
-				// Only prevent default if animation is actually gonna happen
-				event.preventDefault();
-				$("html, body").animate(
-					{
-						scrollTop: target.offset().top
-					},
-					1000,
-					function() {
-						// Callback after animation
-						// Must change focus!
-						var $target = $(target);
-						$target.focus();
-						if ($target.is(":focus")) {
-							// Checking if the target was focused
-							return false;
-						} else {
-							$target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
-							$target.focus(); // Set focus again
-						}
-					}
-				);
-			}
-		}
-	});
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function (event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top,
+          },
+          1000,
+          function () {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) {
+              // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            }
+          }
+        );
+      }
+    }
+  });
+
+function formatPhoneNumber() {
+  let value = $("#number").val();
+  value = value.replace(/\D/g, "");
+
+  if (value.length >= 10) {
+    value = value.replace(/(\d{3})(\d{3})(\d{4})/, "($1) $2-$3");
+  }
+
+  $("#number").val(value);
+}
+$("#number").on("input", formatPhoneNumber);
+
+function emailOrNumber() {
+  const phoneNumber = $("#number").val();
+  const email = $("#email").val();
+  const isEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (phoneNumber.length !== 14 && !isEmail.test(email)) {
+    $("#number").attr("required", true);
+    $("#email").attr("required", true);
+  } else {
+    $("#number").attr("required", false);
+    $("#email").attr("required", false);
+  }
+}
+
+$("form").on("input", emailOrNumber);
